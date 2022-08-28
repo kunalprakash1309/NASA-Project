439fbcf6edd9eee58e3dc9931b9d7badb1039cf6
kunalprakash1309 <kunalprakash1309@gmail.com>
2022-08-28 17:11:09 +0530

    Added launches pagination

--- server/src/routes/launches/launches.contoller.js
+++ server/src/routes/launches/launches.contoller.js
@@ -5,10 +5,12 @@ const {
   abortLaunchById, 
 } = require('../../models/launches.model');
 
+const { getPagination } = require('../../services/query');
+
 async function httpGetAllLaunches(req, res) {
-  // convert MAP object to array. Because res.json() takes certain types. eg array, plain object
-  // we need to convert it to json readable by converting to array(by [...lauches.value()] or below option)
-  return res.status(200).json(await getAllLaunches());
+  const { skip, limit } = getPagination(req.query);
+  const launches = await getAllLaunches(skip, limit);
+  return res.status(200).json(launches);
 };
 
 async function httpAddNewLaunch(req, res) {
